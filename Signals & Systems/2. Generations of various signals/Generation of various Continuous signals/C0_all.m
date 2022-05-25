300b9b0940fcbfe69435e794f21431a2310ad39f
Koneti Raj kumar <106098401+Koneti-Rajkumar@users.noreply.github.com>
2022-05-25 18:05:15 +0530

    Update C0_all.m

--- Signals & Systems/2. Generations of various signals/Generation of various Continuous signals/C0_all.m
+++ Signals & Systems/2. Generations of various signals/Generation of various Continuous signals/C0_all.m
@@ -1,81 +1,123 @@
-
-% 1.impluse
-t=-5:0.01:5;
-x=1.*(t==0);
-subplot(3,3,1)
-plot(t,x)
-xlabel('time')
-ylabel('amplitude')
-title('impulse')
-
-% 2.step
-t=-5:0.01:5;
-x=1.*(t>=0)+0.*(t<0);
-subplot(3,3,2)
-plot(t,x)
-xlabel('time')
-ylabel('amplitude')
-title('step')
-
-% 3.ramp
-t=-5:0.01:5;
-x=t.*(t>=0)+0.*(t<0);
-subplot(3,3,3)
-plot(t,x)
-xlabel('time')
-ylabel('amplitude')
-title('ramp')
-
-% 4.sawtooth
-%t=-pi:0.01:pi;
-%x = sawtooth(t);
-%subplot(3,3,4)
-%plot(t,x)
-%xlabel('time')
-%ylabel('amplitude')
-%title('sawtooth')
-
-% 5.triangular
-t=-5:0.01:5;
-x=t.*(t<=0)-t.*(t>=0);
-subplot(3,3,5)
-plot(t,x)
-xlabel('time')
-ylabel('amplitude')
-title('triangle')
-
-% 4.square
-%t = 2*pi:0.01:2*pi;
-%x=square(t);
-%subplot(3,3,6)
-%plot(t,x)
-%xlabel('time')
-%ylabel('amplitude')
-%title('square')
-
-% 7.sinc
-t=-2*pi:0.01:2*pi;
-x=sinc(t);
-subplot(3,3,7)
-plot(t,x)
-xlabel('time')
-ylabel('amplitude')
-title('sinc')
-
-% 8.sinusoidal
-t=-2*pi:0.01:2*pi;
-x= sin(t);
-subplot(3,3,8)
-plot(t,x)
-xlabel('time')
-ylabel('amplitude')
-title('sinusoidal')
-
-% 9.random
-%t=1;
-%x=rand(t);
-%subplot(3,3,9)
-%plot(t,x)
-%xlabel('time')
-%ylabel('amplitude')
-%title('random')
+%1. unit impulse
+t=-5:0.1:5;
+x1=1.*(t==0);
+x2=0.*(t~=0);
+x=x1+x2;
+subplot(4,3,1);
+plot(t,x);
+xlabel('time ');
+ylabel('amplitude');
+title('unit impulse signal');
+ 
+% 2.unit step
+t=-5:0.1:5;
+x1=1.*(t>=0);
+x2=0.*(t<0);
+x=x1+x2;
+subplot(4,3,2);
+plot(t,x);
+xlabel('time');
+ylabel('amplitude');
+title('unit step signal');
+ 
+%3. unitramp
+t=-5:0.1:5;
+x1=t.*(t>=0);
+x2=0.*(t<0);
+x=x1+x2;
+subplot(4,3,3);
+plot(t,x);
+xlabel('time ');
+ylabel('amplitude');
+title('unit ramp signal');
+ 
+%4. unitparabolic
+t=-5:0.1:5;
+x1=t.^2/2.*(t>=0);
+x2=0.*(t<0);
+x=x1+x2;
+subplot(4,3,4);
+plot(t,x);
+xlabel('time');
+ylabel('amplitude');
+title('unit parabolic signal');
+ 
+%5. rectangular pulse
+t=-5:0.1:5;
+x1=1.*(abs(t)<=0.5);
+x2=0.*(abs(t)>0.5);
+x=x1+x2;
+subplot(4,3,5);
+plot(t,x);
+xlabel('time index t(sec)');
+ylabel('amplitude');
+title('rectangular pulse');
+ 
+%6. triangular pulse
+t=-5:0.1:5;
+x1=(1-abs(t)/4).*(abs(t)<=4);
+x2=0.*(abs(t)>4);
+x=x1+x2;
+subplot(4,3,6);
+plot(t,x);
+xlabel('time');
+ylabel('amplitude');
+title('triangular pulse');
+ 
+%7. signum func
+t=-5:0.1:5;
+x1=1.*(t>=0);
+x2=-1.*(t<0);
+x=x1+x2;
+subplot(4,3,7);
+plot(t,x);
+xlabel('time');
+ylabel('amplitude');
+title('signum functon');
+ 
+%8) sinc func
+t=-pi:0.01:pi;
+x=sinc(t)
+subplot(4,3,8);
+plot(t,x);
+xlabel('time');
+ylabel('amplitude');
+title('sinc function');
+ 
+%9) gaussian func
+t=-5:0.1:5;
+x=exp(-5*t.^2);
+subplot(4,3,9);
+plot(t,x);
+xlabel('time');
+ylabel('amplitude');
+title('gaussian function');
+ 
+%10) sinusoidal signal
+t=-pi:0.1:pi;
+x=sin(t);
+subplot(4,3,10);
+plot(t,x);
+xlabel('time');
+ylabel('amplitude');
+title('sinosoidal signal');
+ 
+%11) real exponential
+t=-5:0.1:5;
+x=4*exp(-5*t);
+subplot(4,3,11);
+plot(t,x);
+xlabel('time index t(sec)');
+ylabel('amplitude');
+title('real exponential');
+ 
+%12.sawtooth wave
+f=50;
+T=1/f;
+t=0:3*T/100:3*T;
+x=sawtooth(2*pi*f*t);
+subplot(4,3,12);
+plot(t,x);
+xlabel('time ');
+ylabel('amplitude');
+title('sawtooth wave signal');
